e6c16f9af20cd9b063b8aa64845b163c223b61a6
JoaoBianco <joaops.bianco@gmail.com>
2021-01-27 08:06:09 -0300

    Modificacao de arquivos

--- config/server.js
+++ config/server.js
@@ -1,16 +1,15 @@
 var express = require("express");
 var consign = require("consign");
 var bodyParser = require("body-parser");
-
-var { check, expressValidator } = require("express-validator");
+var expressValidator = require("express-validator");
 
 var app = express();
 app.set("view engine", "ejs");
 app.set("views", "./app/views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
 
-app.use(check);
 consign()
   .include("app/routes")
   .then("config/dbConnection.js")
