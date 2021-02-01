959349776b86698653ec6af56ca079808ff319f7
brmgf <a16010@bcc.unifal-mg.edu.br>
2021-02-01 15:01:37 -0300

    conectando ao banco

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,14 +1,17 @@
 var mysql = require("mysql");
 
-var connMySQL = () => {
-  return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "portal_noticias",
-  });
-};
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-east-03.cleardb.com',
+  user     : 'b2bdf5e20ca626',
+  password : '36b97c49',
+  database : 'heroku_aba9054eba0552f'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Conectado ao banco");
+});
 
 module.exports = () => {
-  return connMySQL;
+  return connection;
 };
