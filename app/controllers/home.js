88791b90d9895296605376af73aa4cd9363c0567
JoaoBianco <joaops.bianco@gmail.com>
2021-02-01 15:09:45 -0300

    Acesso ao BD

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,8 +1,11 @@
 module.exports.index = (app, req, res) => {
-  var connection = app.config.dbConnection();
-  var noticiasModel = new app.app.models.noticiasDAO(connection);
+  //var connection = app.config.dbConnection();
+  //var noticiasModel = new app.app.models.noticiasDAO(connection);
 
-  noticiasModel.get5UltimasNoticias((error, result) => {
-    res.render("home/index", { noticias: result });
-  });
+  /*noticiasModel.get5UltimasNoticias((error, result) => {
+  res.render("home/index", { noticias: result });
+
+   });
+   */
+  res.render("home/index");
 };
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,15 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host     : 'us-cdbr-east-03.cleardb.com',
-  user     : 'b2bdf5e20ca626',
-  password : '36b97c49',
-  database : 'heroku_aba9054eba0552f'
+  host: "us-cdbr-east-03.cleardb.com",
+  user: "b2bdf5e20ca626",
+  password: "36b97c49",
+  database: "heroku_aba9054eba0552f",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Conectado ao banco");
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Conectado ao banco");
 });
 
 module.exports = () => {
