d4b7f0a0aea1fa9052a7c24f2bac2cfad2ffe71a
JoaoBianco <joaops.bianco@gmail.com>
2021-02-02 18:38:21 -0300

    modificacao do arquivo BD

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -7,11 +7,31 @@ var connection = mysql.createConnection({
   database: "heroku_aba9054eba0552f",
 });
 
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Conectado ao banco");
-});
+var connection;
 
-module.exports = () => {
-  return connection;
-};
+function handleDisconnect() {
+  connection = mysql.createConnection("dbConnection.js"); // Recreate the connection, since
+  // the old one cannot be reused.
+
+  connection.connect(function (err) {
+    // The server is either down
+    if (err) {
+      // or restarting (takes a while sometimes).
+      //console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    } // to avoid a hot loop, and to allow our node script to
+  }); // process asynchronous requests in the meantime.
+  // If you're also serving http, display a 503 error.
+  connection.on("error", function (err) {
+    //console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      // Connection to the MySQL server is usually
+      handleDisconnect(); // lost due to either server restart, or a
+    } else {
+      // connnection idle timeout (the wait_timeout
+      throw err; // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
