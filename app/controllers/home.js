1f690f7d1ed190542f7ace940613f95c6a4e6caf
JoaoBianco <joaops.bianco@gmail.com>
2021-02-11 11:23:02 -0300

    start Inicio

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,14 +1,15 @@
 module.exports.index = (app, req, res) => {
-  var connection = app.config.dbConnection();
-  var teste = new app.app.models.noticiasDAO(connection);
+  //var connection = app.config.dbConnection();
+  //var teste = new app.app.models.noticiasDAO(connection);
 
   /*noticiasModel.get5UltimasNoticias((error, result) => {
   res.render("home/index", { noticias: result });
 
    });
    */
-  teste.getAll((error, result) => {
-    res.render("home/index", { teste: result });
-    console.log(result);
-  });
+  // teste.getAll((error, result) => {
+  //    res.render("home/index", { teste: result });
+  res.render("home/index");
+  //  console.log(result);
+  //});
 };
