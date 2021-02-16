af400e4a8621ca6233e90751a5b068ff81758fd3
JoaoBianco <joaops.bianco@gmail.com>
2021-02-16 11:32:27 -0300

    mudancas no index, modal e listagem de doces

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,14 +1,14 @@
 module.exports.index = (app, req, res) => {
-  // var connection = app.config.dbConnection();
-  // var teste = new app.app.models.teste(connection);
-  // /*noticiasModel.get5UltimasNoticias((error, result) => {
-  // res.render("home/index", { noticias: result });
-  //  });
-  //  */
-  // console.log(connection);
-  // teste.getAll((error, result) => {
-  //   console.log(result);
-  //   res.render("home/index", { teste: result });
-  // });
-  res.render("home/index");
+  var connection = app.config.dbConnection();
+  var teste = new app.app.models.teste(connection);
+  /*noticiasModel.get5UltimasNoticias((error, result) => {
+  res.render("home/index", { noticias: result });
+   });
+   */
+  console.log(connection);
+  teste.getAll((error, result) => {
+    console.log(result);
+    res.render("home/index", { produto: result });
+  });
+  //res.render("home/index");
 };
