ea8ad8ad4962571970f5d991d068d73b70a0f003
JoaoBianco <joaops.bianco@gmail.com>
2021-03-03 15:09:59 -0300

    modificacoes carrinho

--- app/models/home.js
+++ app/models/home.js
@@ -1,40 +1,3 @@
-/*class NoticiasDAO {
-  constructor(connection) {
-    this._connection = connection;
-  }
-
-  getNoticias = (callback) => {
-    this._connection.query(
-      "select * from noticias order by data_criacao desc",
-      callback
-    );
-  };
-
-  getNoticia = (id_noticia, callback) => {
-    let id_da_noticia = id_noticia.id_noticia;
-    this._connection.query(
-      "select * from noticias where id_noticia = " + id_da_noticia,
-      callback
-    );
-  };
-
-  salvarNoticia = (noticia, callback) => {
-    this._connection.query("insert into noticias set ?", noticia, callback);
-  };
-
-  get5UltimasNoticias = (callback) => {
-    this._connection.query(
-      "select * from noticias order by data_criacao desc limit 5",
-      callback
-    );
-  };
-}
-
-module.exports = () => {
-  return NoticiasDAO;
-};
-*/
-
 class produto {
   constructor(connection) {
     this._connection = connection;
@@ -42,16 +5,19 @@ class produto {
   getAll = (callback) => {
     this._connection.query("select * from produto", callback);
   };
-  
+
   getFilter = (id, callback) => {
-    this._connection.query("select * from produto join produtoCategoria on produto.id = produtoCategoria.idProduto where produtoCategoria.idCategoria = ?", id, callback);
+    this._connection.query(
+      "select * from produto join produtoCategoria on produto.id = produtoCategoria.idProduto where produtoCategoria.idCategoria = ?",
+      id,
+      callback
+    );
   };
 
   search = (query, callback) => {
-    const queryString = `SELECT * FROM produto WHERE nome LIKE '%${query}%'`
-    this._connection.query(queryString.toString(), callback)
-  }
-
+    const queryString = `SELECT * FROM produto WHERE nome LIKE '%${query}%'`;
+    this._connection.query(queryString.toString(), callback);
+  };
 }
 
 module.exports = () => {
