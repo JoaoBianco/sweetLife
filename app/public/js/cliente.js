fe79ee9d639cc0afb948cfb974b2efcf4ddab26e
JoaoBianco <joaops.bianco@gmail.com>
2021-03-18 10:51:54 -0300

    Modificacoes Cliente

--- app/public/js/cliente.js
+++ app/public/js/cliente.js
@@ -1,6 +1,24 @@
 const dados = document.querySelector("#meus-dados");
 const dadosBotao = document.querySelector("#aside-dados");
+const pedidos = document.querySelector("#meus-pedidos");
+const pedidosBotao = document.querySelector("#aside-pedidos");
+const enderecos = document.querySelector("#meus-enderecos");
+const enderecosBotao = document.querySelector("#aside-enderecos");
 
 dadosBotao.addEventListener("click", () => {
-  dados.classList.toggle("esconder");
+  dados.classList.remove("esconder");
+  pedidos.classList.add("esconder");
+  enderecos.classList.add("esconder");
+});
+
+pedidosBotao.addEventListener("click", () => {
+  dados.classList.add("esconder");
+  pedidos.classList.remove("esconder");
+  enderecos.classList.add("esconder");
+});
+
+enderecosBotao.addEventListener("click", () => {
+  dados.classList.add("esconder");
+  pedidos.classList.add("esconder");
+  enderecos.classList.remove("esconder");
 });
